e9174cf34a8c12415e2b88a094cb079859fde440
Dalyl Feddal <feddal.dalyl@yahoo.fr>
2017-04-27 16:32:04 +0200

    partial event with delay depassed and current

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,9 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events_depassed = Event.where("schedule_at < ? ",
+    DateTime.now)
+    @events_current = Event.where("schedule_at > ?", DateTime.now)
   end
 
   # GET /events/1
