4b6d1906c0882ecab8e7122528242b96fef3dc7a
Dalyl Feddal <feddal.dalyl@yahoo.fr>
2017-05-10 17:51:35 +0200

    fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  has_many :event_users
+  has_many :events, through: :event_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :event_users
-has_many :events, through: :event_users
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
